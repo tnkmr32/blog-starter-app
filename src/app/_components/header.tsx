17e9b47dbb92a71d0e8118df8ce7bbf03b163335
t-nakamura <tnakkrkr@gmail.com>
2025-04-01 20:27:12 +0900

    ヘッダー　余白調整

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -4,9 +4,9 @@ import styles from "./header.module.css";
 
 const Header = () => {
   return (
-    <div className="pt-8 bg-neutral-50 border-t border-neutral-200 dark:bg-slate-800">
+    <div className="pt-8 bg-neutral-50 border-b border-neutral-200 dark:bg-slate-800 mb-10">
       <div className="container mx-auto max-w-5xl px-10">
-      <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight pb-10 mb-10 flex items-center">
+      <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight pb-10 flex items-center">
         <Link href="/" className="hover:underline">
         {SITE_NAME}
         </Link>
