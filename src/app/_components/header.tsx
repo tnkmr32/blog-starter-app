0d66d30ba4b074ced6305bd2c7db522618bafc5d
t-nakamura <tnakkrkr@gmail.com>
2025-04-01 20:08:43 +0900

    ヘッダー修正

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,10 +1,11 @@
 import { SITE_NAME } from "@/lib/constants";
 import Link from "next/link";
+import styles from "./header.module.css";
 
 const Header = () => {
   return (
-    <div className="pt-8 bg-gray-200">
-      <div className="container mx-auto px-10">
+    <div className={styles.header}>
+      <div className="container mx-auto max-w-5xl px-10">
       <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight pb-10 mb-10 flex items-center">
         <Link href="/" className="hover:underline">
         {SITE_NAME}
