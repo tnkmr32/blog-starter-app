88927e6a293db59602fac18a0f28ef4d16abd52a
t-nakamura <tnakkrkr@gmail.com>
2025-05-06 14:36:26 +0900

    ページ仮実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import { HeroPost } from "@/app/_components/hero-post";
 import { Intro } from "@/app/_components/intro";
 import { MoreStories } from "@/app/_components/more-stories";
 import { getAllPosts } from "@/lib/api";
+import PortfolioItem from "./_components/portfolio-item";
+import Link from "./_components/link";
 
 export default function Index() {
   const allPosts = getAllPosts();
@@ -13,17 +15,24 @@ export default function Index() {
 
   return (
     <main>
+      <div className="overflow-x-auto">
+        <div className="flex gap-sm p-sm w-max">
+          {/* TODO: 配列で受け取って一覧表示にする */}
+          <PortfolioItem title={"AAAA"} src={""} />
+          <PortfolioItem title={"BBB"} src={""} />
+          <PortfolioItem title={"AAAA"} src={""} />
+          <PortfolioItem title={"AAAA"} src={""} />
+          <PortfolioItem title={"AAAA"} src={""} />
+        </div>
+      </div>
       <Container>
-        <Intro />
-        <HeroPost
-          title={heroPost.title}
-          coverImage={heroPost.coverImage}
-          date={heroPost.date}
-          author={heroPost.author}
-          slug={heroPost.slug}
-          excerpt={heroPost.excerpt}
-        />
-        {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+        <div className="flex flex-row justify-end">
+          <Link href={"/"} className="text-accent w-max">
+            {">>Portfolio"}
+          </Link>
+        </div>
+        <div>ブログ</div>
+        <div>ブログ一覧へのリンク</div>
       </Container>
     </main>
   );
